164a43bd9458017405dc2e9397b6c32f47716914
Vikrant Singh <vikrantsinghvm@gmail.com>
2016-08-04 03:57:31 -0400

    Removed case sensitivity from keywords and messages

--- Run.py
+++ Run.py
@@ -6,20 +6,24 @@ s = open_socket()
 join_room(s)
 
 
-counter = 1
+
 while(True):
   read_buffer = s.recv(1024).decode('UTF-8')
   reply_to_user = get_user(read_buffer)
-  reply_to_message = get_message(read_buffer)
+  reply_to_message = get_message(read_buffer).lower()
 
-  if (config.TARGET_KEYWORD in reply_to_message):
+  if (config.TARGET_KEYWORD and config.TARGET_KEYWORD.lower() in reply_to_message):
     if(not config.TARGET_USER):
       send_message(s, config.SPAM_MESSAGE)
     else:
       send_message(s,  "@" + reply_to_user + " " + config.SPAM_MESSAGE)
-
-  counter += 1
-  time.sleep(5)
+  elif(not config.TARGET_KEYWORD):
+    if(not config.TARGET_USER):
+      send_message(s, config.SPAM_MESSAGE)
+    else:
+      send_message(s,  "@" + reply_to_user + " " + config.SPAM_MESSAGE)
+      
+  time.sleep(config.DELAY_BETWEEN_SPAM)
 	
 
 	
