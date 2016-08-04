b08ed503962dfa4fc071e773aece0d7b05efcace
Vikrant Singh <vikrantsinghvm@gmail.com>
2016-08-04 04:06:51 -0400

    Added option to add delay between spam, to avoid ban

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
 
-### Connection and authentication
+### Connection and authentication =============================
 HOST = "irc.chat.twitch.tv."
 PORT = 6667
-CHANNEL = "dunkoreosinmilk"
-PASS = "oauth:oes2snkaipxdxfqxkfsxg04gr2dvk3"
-USER = "DunkOreosInMilk"
+CHANNEL = "" #name of the channel chat you want to spam 
+PASS = "" #OAuth key for twitch or similar api, http://twitchapps.com/tmi/
+USER = "" #your or your bot's twitch username
 
-### Spam related constants
+### Spam related constants ===================================
 
-### Change TARGET_KEYWORD to "False" without the quotes if you don't want to
-### reply to a specific keyword and just want to spam links, etc. 
+### Change TARGET_KEYWORD to "False", without the quotes, if you don't want to
+### reply to a specific keyword, and just want to spam links, etc. 
 TARGET_KEYWORD = "hello"
 
 ### TARGET_USER only takes "True" or "False" without the quotes
 TARGET_USER = True
 
-SPAM_MESSAGE = "hello" 
+SPAM_MESSAGE = "hello"
+DELAY_BETWEEN_SPAM = 5 #SECONDS, set it to 0 at your own risk.
