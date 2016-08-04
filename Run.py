f89e9fa3a73caa1b3f1102ee01eaaf3af8d72d59
Vikrant Singh <vikrantsinghvm@gmail.com>
2016-08-04 16:21:37 -0400

    Anti Ban with random int added

--- Run.py
+++ Run.py
@@ -10,25 +10,31 @@ join_room(s)
 while(True):
   read_buffer = s.recv(1024).decode('UTF-8')
   print(read_buffer)
+
+  #ping pong 
   if (read_buffer.strip(" \n\r") == "PING :tmi.twitch.tv"):
     send_message(s, "PONG :tmi.twitch.tv")
     continue
+
+  #get user and message
   reply_to_user = get_user(read_buffer)
   reply_to_message = get_message(read_buffer).lower()
-  
+
+  #anti-ban random int
+  random_int = random.randrange(0,1000000) if config.ANTI_BAN_RANDOM_INT else ""
 	
   if (config.TARGET_KEYWORD != []):
     for keyword in config.TARGET_KEYWORD:
       if (keyword.lower() in reply_to_message):
         if(not config.TARGET_USER):
-          send_message(s, config.SPAM_MESSAGE)
+          send_message(s, config.SPAM_MESSAGE + " " + str(random_int))
         else:
-          send_message(s,  "@" + reply_to_user + " " + config.SPAM_MESSAGE)
+          send_message(s,  "@" + reply_to_user + " " + config.SPAM_MESSAGE + " " + str(random_int))
         break
   else:
     if(not config.TARGET_USER):
-        send_message(s, config.SPAM_MESSAGE)
+        send_message(s, config.SPAM_MESSAGE + " " + str(random_int))
     else:
-        send_message(s,  "@" + reply_to_user + " " + config.SPAM_MESSAGE)
+        send_message(s,  "@" + reply_to_user + " " + config.SPAM_MESSAGE + " " + str(random_int))
     
   time.sleep(config.DELAY_BETWEEN_SPAM)	
--- config.py
+++ config.py
@@ -1,19 +1,30 @@
 
 ### Connection and authentication =============================
+
 HOST = "irc.chat.twitch.tv."
 PORT = 6667
-CHANNEL = "" #name of the channel chat you want to spam 
-PASS = "" #OAuth key for twitch or similar api, http://twitchapps.com/tmi/
-USER = "" #your or your bot's twitch username
+#name of the channel chat you want to spam 
+CHANNEL = "loltyler1"
+#OAuth key for twitch or similar api, http://twitchapps.com/tmi/
+PASS = ""
+#your or your bot's twitch username
+USER = "" 
 
 ### Spam related constants ===================================
 
 ### Change TARGET_KEYWORD to [], if you don't want to
 ### reply to a specific keyword, and just want to spam links, etc. 
-TARGET_KEYWORD = ["hello", "Kappa"]
+TARGET_KEYWORD = ["hello", "Kappa", "elo", "BibleThump", "PogChamp",
+                  "alpha", "fag", "kys", "banned", "stream", "tyler"]
 
 ### TARGET_USER only takes "True" or "False" without the quotes
 TARGET_USER = True
 
-SPAM_MESSAGE = "Hello! Kappa"
-DELAY_BETWEEN_SPAM = 5 #SECONDS, set it to 0 at your own risk.
+SPAM_MESSAGE = "BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa BANNED Kappa "
+
+### Anti Ban===================================================
+
+#SECONDS, set it to 0 at your own risk.
+DELAY_BETWEEN_SPAM = 3 
+#only take "True" or "False", without the quotes. Adds a random int after spam.
+ANTI_BAN_RANDOM_INT = True
